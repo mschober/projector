7757e535d90180267ebf22a0a470e6afb85065c8
Mike Schober <mschober@cj.com>
2012-10-06 15:12:47 -0700

    create and deleting handled

--- create.py
+++ create.py
@@ -3,10 +3,23 @@ import os
 
 script, app_name = argv
 
-if not os.path.exists(app_name + "/src"):
-    os.makedirs(app_name + "/src")
+def create_directories(app_name):
+    if not os.path.exists(app_name + "/src"):
+        os.makedirs(app_name + "/src")
 
-if not os.path.exists(app_name + "/test"):
-    os.makedirs(app_name + "/test")
+    if not os.path.exists(app_name + "/test"):
+        os.makedirs(app_name + "/test")
 
+def destroy_directories(app_name):
+    if os.path.exists(app_name + "/src"):
+        os.removedirs(app_name + "/src")
 
+    if os.path.exists(app_name + "/test"):
+        os.removedirs(app_name + "/test")
+
+print "Creating app structure"
+create_directories(app_name)
+os.system("tree %r" % app_name)
+
+print "Deleting app structure"
+destroy_directories(app_name)
